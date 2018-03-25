96098273b5378fb2fe7b1458297603d03b29077f
Maximilian Fuchs <maximilian.fuchs@stud.tu-darmstadt.de>
2018-03-25 22:41:56 +0200

    Added methods to support XMI Strings and Typesystem Strings

--- pycas/pycas/cas/writer/XmiWriter.py
+++ pycas/pycas/cas/writer/XmiWriter.py
@@ -16,13 +16,13 @@ class XmiWriter(object):
         '''
         Constructor
         '''
-        #name space qualifier of the root XMI element 
+        #name space qualifier of the root XMI element
         self.__tagQualifier = 'xmi'
         #name of the root XMI element
         self.__tagName = 'XMI'
         #version of the root XMI element
         self.__version = '2.0'
-        #dictionary of XML name space, used to populate the name spaces in root XMI element 
+        #dictionary of XML name space, used to populate the name spaces in root XMI element
         self.__xmlnsDict = {}
         #list of name spaces
         self.__xmlnsDictList = []
@@ -31,28 +31,33 @@ class XmiWriter(object):
         #same second last domain qualifier, but if the qualifiers up to the second last domain qualifier does not match
         #then they are considered as different types and must have given a unique name space
         #if the second last domain qualifier and qualifiers up to the second last domain qualifier matches then it considered
-        #under same name space     
+        #under same name space
         self.__TagQualifierCounter=1
-    "This method writes the CAS as a XMI file to the given path(file name also included in the path)"    
+
+    "This method returns the CAS as a XMI String" 
+    def writeToString(self, cas):
+           return self.__toString(cas)
+
+    "This method writes the CAS as a XMI file to the given path(file name also included in the path)"
     def write(self,cas,filepath):
         try:
             if not (os.path.dirname(filepath) == ""):
                 os.makedirs(os.path.dirname(filepath), exist_ok=True)
             with open(filepath, "w") as f:
-                f.write(self.__toString(cas))        
+                f.write(self.__toString(cas))
         except:
             print('Something went wrong! cannot create file',os.path.basename(filepath),'in',os.path.dirname(filepath))
             sys.exit(1)
-    "This internal method generates Strings from the CAS object so that it can be written"    
+    "This internal method generates Strings from the CAS object so that it can be written"
     def __toString(self,cas):
-        #get the unique types from type system to build the name spaces 
+        #get the unique types from type system to build the name spaces
         nslist = []
         for typeName in cas.typesystem.getAllTypeDesc():
             if (not typeName.name in nslist):
                 nslist.append(typeName.name)
         #build the name space dictionary, contains name space, name space qualifier and url
-        #url is created from name space to be added in root element of XMI with the name space qualifier  
-        
+        #url is created from name space to be added in root element of XMI with the name space qualifier
+
         #holds the list of unique urls
         urlList = []
         # holds list of name space qualifier already generated
@@ -61,7 +66,7 @@ class XmiWriter(object):
         for ns in nslist:
             domainurl = 'http:///'+ns[0:ns.rfind(".")].replace('.','/')+'.ecore'
             Secondlastval=ns.split('.')[len(ns.split('.'))-2]
-            tagQualifier=Secondlastval 
+            tagQualifier=Secondlastval
             while True:
                 #if domain url already exists do not add
                 if domainurl in urlList:
@@ -72,26 +77,26 @@ class XmiWriter(object):
                     if not tagQualifier in tagList:
                         self.__xmlnsDictList.append({'namespace': ns[0:ns.rfind(".")], 'tagQualifier': tagQualifier, 'domainurl': domainurl})
                         urlList.append(domainurl)
-                        tagList.append(tagQualifier) 
+                        tagList.append(tagQualifier)
                         break
                     else:
                         self.__TagQualifierCounter = self.__TagQualifierCounter + 1
                         tagQualifier= Secondlastval+str(self.__TagQualifierCounter)
-        #add the name space for the XMI to xmlnsDictList 
-        self.__xmlnsDictList.append({'namespace': 'www.omg.org', 'tagQualifier': 'xmi', 'domainurl': 'http://www.omg.org/XMI'})                
-            
+        #add the name space for the XMI to xmlnsDictList
+        self.__xmlnsDictList.append({'namespace': 'www.omg.org', 'tagQualifier': 'xmi', 'domainurl': 'http://www.omg.org/XMI'})
+
         #add XMI root tag name and qualifier
-        xmistr = '<?xml version="1.0" encoding="UTF-8"?>'+'<'+self.__tagQualifier+':'+self.__tagName 
-        
+        xmistr = '<?xml version="1.0" encoding="UTF-8"?>'+'<'+self.__tagQualifier+':'+self.__tagName
+
         #add name spaces
         for nselem in self.__xmlnsDictList:
             xmistr =xmistr+' xmlns:'+nselem['tagQualifier']+'="'+nselem['domainurl']+'"'
         #close root tag with version
         xmistr = xmistr+' xmi:version="'+self.__version+'">'
-        
+
         #add casnull
         xmistr = xmistr+'<cas:NULL xmi:id="0"/>'
-        
+
         fsNotInIndexListCheck= []
         indexedFsIdArray = []
         #get the list of annotations or FS in the CAS
@@ -105,7 +110,7 @@ class XmiWriter(object):
                 #add the id to the indexedFsIdArray to record that this FS is already processed
                 indexedFsIdArray.append(element.FSid)
                 xmistr = xmistr+casnull
-            
+
             #add cas:sofa element
             if( element.FStype.name == 'uima.cas.Sofa'):
                 domlist=element.FStype.name.split('.')
@@ -115,7 +120,7 @@ class XmiWriter(object):
                 #add the id to the indexedFsIdArray to record that this FS is already processed
                 indexedFsIdArray.append(int(element.FSid))
                 xmistr = xmistr+cassofa
-            
+
             #add other feature structures
             else:
                 domlist=element.FStype.name.split('.')
@@ -128,7 +133,7 @@ class XmiWriter(object):
                 sofa=''
                 begin=''
                 end=''
-                #if the FS has features iterate through list of features and serialize 
+                #if the FS has features iterate through list of features and serialize
                 if(len(element.getFeatures()) >= 1):
                     for fdict in element.getFeatureValsAsDictList():
                         for fname,fval in fdict.items():
@@ -141,11 +146,11 @@ class XmiWriter(object):
                             #get the end
                             elif(fname == 'end'):
                                 end = ' end="' +str(fval)+'"'
-                            #for all other features    
+                            #for all other features
                             else:
-                                #if feature value is not a list 
+                                #if feature value is not a list
                                 if not type(fval) is list:
-                                    #if its FS take FSid and add the FS to the fsNotInIndexListCheck 
+                                    #if its FS take FSid and add the FS to the fsNotInIndexListCheck
                                     if (isinstance(fval,TypeDescription) and isinstance(fval,FeatureStructure)):
                                         fspoprs=fspoprs+' '+fname+'="'+str(fval.FSid)+'"'
                                         fsNotInIndexListCheck.append(fval)
@@ -159,21 +164,21 @@ class XmiWriter(object):
                                         #if its FS take fs id and add the FS to the fsNotInIndexListCheck
                                         if (isinstance(valelement,TypeDescription) and isinstance(valelement,FeatureStructure)):
                                             listval = str(valelement.FSid) if (listval == '') else listval+' '+str(valelement.FSid)
-                                            fsNotInIndexListCheck.append(valelement)  
+                                            fsNotInIndexListCheck.append(valelement)
                                         else:
-                                            listval = str(valelement) if (listval == '') else listval+' '+str(valelement) 
-                                    fspoprs=fspoprs+' '+fname+'="'+listval+'"'        
+                                            listval = str(valelement) if (listval == '') else listval+' '+str(valelement)
+                                    fspoprs=fspoprs+' '+fname+'="'+listval+'"'
                                 else:
                                     raise TypeError('type of feature',fname,'not recognized')
-                                            
+
                 #add attributes with vale
                 casfs= casfs + sofa + begin + end +  fspoprs
                 #add closing
                 casfs = casfs+'/>'
-                
+
                 xmistr = xmistr + casfs
         #Now check the FS added in fsNotInIndexListCheck if they are processed means in indexedFsIdArray ignore, otherwise process
-        #this is done because all the referenced FS are not added in index of the CAS          
+        #this is done because all the referenced FS are not added in index of the CAS
         for fs in fsNotInIndexListCheck:
             if( not fs.FSid in indexedFsIdArray):
                 domlist=fs.FStype.name.split('.')
@@ -210,20 +215,20 @@ class XmiWriter(object):
                                         if (isinstance(valelement,TypeDescription) and isinstance(valelement,FeatureStructure)):
                                             listval = str(valelement.FSid) if (listval == '') else listval+' '+str(valelement.FSid)
                                         else:
-                                            listval = str(valelement) if (listval == '') else listval+' '+str(valelement) 
-                                    fspoprs=fspoprs+' '+fname+'="'+listval+'"'        
+                                            listval = str(valelement) if (listval == '') else listval+' '+str(valelement)
+                                    fspoprs=fspoprs+' '+fname+'="'+listval+'"'
                                 else:
                                     raise TypeError('type of feature',fname,'not recognized')
-                                            
+
                 #add attributes with vale
                 casfs= casfs + sofa + begin + end +fspoprs
                 #add closing
                 casfs = casfs+'/>'
-                
+
                 xmistr = xmistr + casfs
-                
+
         #add cas:View element
-        casview = '<cas:View'+' Sofa="'+cas.sofaFS.FSid+'"'         
+        casview = '<cas:View'+' Sofa="'+cas.sofaFS.FSid+'"'
         memlist=''
         for element in cas.getSofaCasView():
             if memlist == '':
@@ -233,10 +238,10 @@ class XmiWriter(object):
         casview = casview + ' members="'+memlist+'"/>'
 
         xmistr = xmistr + casview
-                
-        #add closing xmi root tag    
+
+        #add closing xmi root tag
         xmistr = xmistr+'</'+self.__tagQualifier+':'+self.__tagName+'>'
-            
+
         return xmistr
     "This method returns the tag qualifier for a given type"
     def __getTagQualifier(self,giventype):
@@ -245,7 +250,3 @@ class XmiWriter(object):
             if nselem['namespace'] == domain:
                 tagQualifier = nselem['tagQualifier']
         return tagQualifier
-                    
-            
-        
-            
--- pycas/pycas/type/cas/TypeSystemFactory.py
+++ pycas/pycas/type/cas/TypeSystemFactory.py
@@ -26,17 +26,17 @@ class TypeSystemFactory(object):
         typesystempath (Str):  path of a type system xml file.
     Returns:
         TypeSystem object
-    '''    
+    '''
     def readTypeSystem(self,typesystempath):
         #create TypeSystem object
-        typesystem = TypeSystem();        
+        typesystem = TypeSystem();
         #create TypeSystemParser object
         typesytemParser = TypeSystemParser()
         # set the type system path to TypeSystemParser
         typesytemParser.setTypeAsFile(typesystempath)
-        # populate TypeSystem name space attribute to contain the base name space 
+        # populate TypeSystem name space attribute to contain the base name space
         typesystem.namespace = typesytemParser.getTypeSystemDescNamespace()
-        
+
         # iterate type description elements returned by the parser and create a typeDescription object for TypeSystem
         for tdelement in typesytemParser.getTypeDescElements():
             # create type description object
@@ -44,7 +44,7 @@ class TypeSystemFactory(object):
             TDdesc=typesytemParser.getElementText(typesytemParser.getTypeDescriptionDescription(tdelement))
             typeDesc = TypeDescription(TDname,TDdesc)
             typeDesc.superType = typesytemParser.getElementText(typesytemParser.getTypeDescriptionSuperType(tdelement))
-            #get the features element from parser 
+            #get the features element from parser
             features = typesytemParser.getTypeDescriptionFeatures(tdelement)
             if not features == None:
                 #get the feature description element from the parser and create features object for typeDescription
@@ -62,33 +62,33 @@ class TypeSystemFactory(object):
                             raise TypeError(fname,'has only rangeType as FSArray, no element type found')
                             return
                         #if range type is primitive CAS type
-                        if(rangeValStr == CAS_Type.TYPE_NAME_BOOLEAN or rangeValStr == CAS_Type.TYPE_NAME_FLOAT or 
-                           rangeValStr == CAS_Type.TYPE_NAME_DOUBLE or rangeValStr == CAS_Type.TYPE_NAME_INTEGER or 
+                        if(rangeValStr == CAS_Type.TYPE_NAME_BOOLEAN or rangeValStr == CAS_Type.TYPE_NAME_FLOAT or
+                           rangeValStr == CAS_Type.TYPE_NAME_DOUBLE or rangeValStr == CAS_Type.TYPE_NAME_INTEGER or
                            rangeValStr == CAS_Type.TYPE_NAME_LONG or rangeValStr == CAS_Type.TYPE_NAME_STRING):
-                        
+
                             #set the range type from type system of the feature
                             feature.rangeType = rangeValStr
                         else:
                             feature.rangeType = TypeDescription(rangeValStr)
-                        
+
                     if(not elementVal == None):
                         elementValStr = typesytemParser.getElementText(elementVal)
                         #if element type is primitive CAS type
-                        if(elementValStr == CAS_Type.TYPE_NAME_BOOLEAN or elementValStr == CAS_Type.TYPE_NAME_FLOAT or 
-                           elementValStr == CAS_Type.TYPE_NAME_DOUBLE or elementValStr == CAS_Type.TYPE_NAME_INTEGER or 
+                        if(elementValStr == CAS_Type.TYPE_NAME_BOOLEAN or elementValStr == CAS_Type.TYPE_NAME_FLOAT or
+                           elementValStr == CAS_Type.TYPE_NAME_DOUBLE or elementValStr == CAS_Type.TYPE_NAME_INTEGER or
                            elementValStr == CAS_Type.TYPE_NAME_LONG or elementValStr == CAS_Type.TYPE_NAME_STRING):
-                        
+
                             #set the range type from type system of the feature
                             feature.elementType = elementValStr
                         else:
                             feature.elementType = TypeDescription(elementValStr)
 
-                    #add the feature to the TypeDescription object                                                
+                    #add the feature to the TypeDescription object
                     typeDesc.addFeature(feature)
             #add the TypeDescription to the TypeSystem object
             typesystem.addTypeDesc(typeDesc)
-            
-            #add inbuilt uima.cas.AnnotationBase type with sofa feature 
+
+            #add inbuilt uima.cas.AnnotationBase type with sofa feature
             typeDescABase = TypeDescription('uima.cas.AnnotationBase','')
             typeDescABase.superType = 'uima.cas.TOP'
             featureSofa = Feature('uima.cas.AnnotationBase','sofa')
@@ -97,7 +97,7 @@ class TypeSystemFactory(object):
             typeDescABase.addFeature(featureSofa)
             typesystem.addTypeDesc(typeDescABase)
 
-            #add inbuilt uima.tcas.Annotation type with begin  and end feature 
+            #add inbuilt uima.tcas.Annotation type with begin  and end feature
             typeDescABase = TypeDescription('uima.tcas.Annotation','')
             typeDescABase.superType = 'uima.cas.AnnotationBase'
             #add begin feature to inbuilt uima.tcas.Annotation
@@ -112,12 +112,12 @@ class TypeSystemFactory(object):
             typeDescABase.addFeature(featureSofa)
             #add inbuilt uima.tcas.Annotation type to the list of type descriptions
             typesystem.addTypeDesc(typeDescABase)
-            
+
             #add inbuilt FSArray type
             typeDescFsArray = TypeDescription('uima.cas.FSArray','')
             typeDescFsArray.superType = 'uima.cas.TOP'
             typesystem.addTypeDesc(typeDescFsArray)
-            
+
             #add inbuilt sofa type
             typeDescSofa = TypeDescription('uima.cas.Sofa','')
             typeDescSofa.superType = 'uima.cas.TOP'
@@ -143,5 +143,125 @@ class TypeSystemFactory(object):
             typeDescSofa.addFeature(featureSofaString)
             #add inbuilt sofa type to the list of type descriptions
             typesystem.addTypeDesc(typeDescSofa)
-            
-        return typesystem 
+
+        return typesystem
+
+
+    def readTypeSystemString(self,typesystemstring):
+        #create TypeSystem object
+        typesystem = TypeSystem();
+        #create TypeSystemParser object
+        typesytemParser = TypeSystemParser()
+        # set the type system path to TypeSystemParser
+        typesytemParser.setTypeAsString(typesystemstring)
+        # populate TypeSystem name space attribute to contain the base name space
+        typesystem.namespace = typesytemParser.getTypeSystemDescNamespace()
+
+        # iterate type description elements returned by the parser and create a typeDescription object for TypeSystem
+        for tdelement in typesytemParser.getTypeDescElements():
+            # create type description object
+            TDname=typesytemParser.getElementText(typesytemParser.getTypeDescriptionName(tdelement))
+            TDdesc=typesytemParser.getElementText(typesytemParser.getTypeDescriptionDescription(tdelement))
+            typeDesc = TypeDescription(TDname,TDdesc)
+            typeDesc.superType = typesytemParser.getElementText(typesytemParser.getTypeDescriptionSuperType(tdelement))
+            #get the features element from parser
+            features = typesytemParser.getTypeDescriptionFeatures(tdelement)
+            if not features == None:
+                #get the feature description element from the parser and create features object for typeDescription
+                for felement in typesytemParser.getFeatureDescElements(features):
+                    fdomain = TDname
+                    fname = typesytemParser.getElementText(typesytemParser.getFeatureDescName(felement))
+                    feature = Feature(fdomain,fname)
+                    feature.description= typesytemParser.getElementText(typesytemParser.getFeatureDescDescription(felement))
+
+                    rangeVal = typesytemParser.getFeatureDescRangeType(felement)
+                    elementVal =  typesytemParser.getFeatureDescElementType(felement)
+                    if(not rangeVal == None):
+                        rangeValStr= typesytemParser.getElementText(rangeVal)
+                        if(rangeValStr == CAS_Type.TYPE_NAME_FSARRAY and elementVal == None ):
+                            raise TypeError(fname,'has only rangeType as FSArray, no element type found')
+                            return
+                        #if range type is primitive CAS type
+                        if(rangeValStr == CAS_Type.TYPE_NAME_BOOLEAN or rangeValStr == CAS_Type.TYPE_NAME_FLOAT or
+                           rangeValStr == CAS_Type.TYPE_NAME_DOUBLE or rangeValStr == CAS_Type.TYPE_NAME_INTEGER or
+                           rangeValStr == CAS_Type.TYPE_NAME_LONG or rangeValStr == CAS_Type.TYPE_NAME_STRING):
+
+                            #set the range type from type system of the feature
+                            feature.rangeType = rangeValStr
+                        else:
+                            feature.rangeType = TypeDescription(rangeValStr)
+
+                    if(not elementVal == None):
+                        elementValStr = typesytemParser.getElementText(elementVal)
+                        #if element type is primitive CAS type
+                        if(elementValStr == CAS_Type.TYPE_NAME_BOOLEAN or elementValStr == CAS_Type.TYPE_NAME_FLOAT or
+                           elementValStr == CAS_Type.TYPE_NAME_DOUBLE or elementValStr == CAS_Type.TYPE_NAME_INTEGER or
+                           elementValStr == CAS_Type.TYPE_NAME_LONG or elementValStr == CAS_Type.TYPE_NAME_STRING):
+
+                            #set the range type from type system of the feature
+                            feature.elementType = elementValStr
+                        else:
+                            feature.elementType = TypeDescription(elementValStr)
+
+                    #add the feature to the TypeDescription object
+                    typeDesc.addFeature(feature)
+            #add the TypeDescription to the TypeSystem object
+            typesystem.addTypeDesc(typeDesc)
+
+            #add inbuilt uima.cas.AnnotationBase type with sofa feature
+            typeDescABase = TypeDescription('uima.cas.AnnotationBase','')
+            typeDescABase.superType = 'uima.cas.TOP'
+            featureSofa = Feature('uima.cas.AnnotationBase','sofa')
+            featureSofa.description= ''
+            featureSofa.elementType = TypeDescription('uima.cas.Sofa')
+            typeDescABase.addFeature(featureSofa)
+            typesystem.addTypeDesc(typeDescABase)
+
+            #add inbuilt uima.tcas.Annotation type with begin  and end feature
+            typeDescABase = TypeDescription('uima.tcas.Annotation','')
+            typeDescABase.superType = 'uima.cas.AnnotationBase'
+            #add begin feature to inbuilt uima.tcas.Annotation
+            featureSofa = Feature('uima.tcas.Annotation','begin')
+            featureSofa.description= ''
+            featureSofa.elementType = CAS_Type.TYPE_NAME_INTEGER
+            typeDescABase.addFeature(featureSofa)
+            #add end feature to inbuilt uima.tcas.Annotation
+            featureSofa = Feature('uima.tcas.Annotation','end')
+            featureSofa.description= ''
+            featureSofa.elementType = CAS_Type.TYPE_NAME_INTEGER
+            typeDescABase.addFeature(featureSofa)
+            #add inbuilt uima.tcas.Annotation type to the list of type descriptions
+            typesystem.addTypeDesc(typeDescABase)
+
+            #add inbuilt FSArray type
+            typeDescFsArray = TypeDescription('uima.cas.FSArray','')
+            typeDescFsArray.superType = 'uima.cas.TOP'
+            typesystem.addTypeDesc(typeDescFsArray)
+
+            #add inbuilt sofa type
+            typeDescSofa = TypeDescription('uima.cas.Sofa','')
+            typeDescSofa.superType = 'uima.cas.TOP'
+            #add sofaNum feature to inbuilt sofa type
+            featureSofaNum = Feature('uima.cas.Sofa','sofaNum')
+            featureSofaNum.description= ''
+            featureSofaNum.elementType = CAS_Type.TYPE_NAME_INTEGER
+            typeDescSofa.addFeature(featureSofaNum)
+            #add sofaID feature to inbuilt sofa type
+            featureSofaID = Feature('uima.cas.Sofa','sofaID')
+            featureSofaID.description= ''
+            featureSofaID.elementType = CAS_Type.TYPE_NAME_STRING
+            typeDescSofa.addFeature(featureSofaID)
+            #add mimeType feature to inbuilt sofa type
+            featureMimeType = Feature('uima.cas.Sofa','mimeType')
+            featureMimeType.description= ''
+            featureMimeType.elementType = CAS_Type.TYPE_NAME_STRING
+            typeDescSofa.addFeature(featureMimeType)
+            #add sofaString feature to inbuilt sofa type
+            featureSofaString = Feature('uima.cas.Sofa','sofaString')
+            featureSofaString.description= ''
+            featureSofaString.elementType = CAS_Type.TYPE_NAME_STRING
+            typeDescSofa.addFeature(featureSofaString)
+            #add inbuilt sofa type to the list of type descriptions
+            typesystem.addTypeDesc(typeDescSofa)
+
+        return typesystem
