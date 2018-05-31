97c4d325727d252d34bd2b4fcf8b5baa78d631a5
mromanello <matteo.romanello@gmail.com>
2018-05-31 22:00:31 +0200

    #4 - Make this a proper python package
    - fixed path to test files throughout tests/TestTypeSystemParser.py

--- tests/TestTypeSystemParser.py
+++ tests/TestTypeSystemParser.py
@@ -8,38 +8,42 @@ unit test cases to check TypeSystemParser functionality
 import unittest
 from pycas.cas.parse.TypeSystemParser import TypeSystemParser
 
-class TestTypeSystemParserMethods(unittest.TestCase):    
-    def test_witeTypeAsFile(self):         
+class TestTypeSystemParserMethods(unittest.TestCase):
+    def test_witeTypeAsFile(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')
-        #provide proper path 
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
+        #provide proper path
         "self.assertEqual(castypesytem.witeTypeAsFile('C:/Users/Dibyojyoti/Desktop/scriptop/typesystem_op.xml'), None)"
-    def test_type(self):        
+
+    def test_type(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         self.assertEqual(type(castypesytem), TypeSystemParser );
         self.assertEqual(isinstance(castypesytem,TypeSystemParser), True );
         self.assertEqual(issubclass(type(castypesytem),str), False );
-    def test_getTypeSystemRootAsString(self):        
+
+    def test_getTypeSystemRootAsString(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         "print(castypesytem.getTypeSystemRoot())"
         self.assertEqual(castypesytem.getTypeSystemRootAsString(), castypesytem.getTypeAsString() );
-    def test_getTypeSystem(self):        
+
+    def test_getTypeSystem(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
 
         self.assertEqual(castypesytem.getTypeSystemDesc(), '{http://uima.apache.org/resourceSpecifier}typeSystemDescription')
         self.assertEqual(castypesytem.getTypeSystemDescNamespace(),'http://uima.apache.org/resourceSpecifier')
         self.assertEqual(castypesytem.getTypeSystemDescLocalname(),'typeSystemDescription')
         "print(castypesytem.getTypeSystemTypes())"
         "print(castypesytem.getTypeSystemTypesAsString())"
-    def test_getTypeDesc(self):        
+
+    def test_getTypeDesc(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')                
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         "print(castypesytem.getTypeDescElements())"
         "print(castypesytem.getTypeDescElements()[0])"
-        self.assertEqual(castypesytem.getTypeDescElementsAsStingList()[0], 
+        self.assertEqual(castypesytem.getTypeDescElementsAsStingList()[0],
         '<typeDescription xmlns="http://uima.apache.org/resourceSpecifier"><name>uima.tcas.DocumentAnnotation</name><description/><supertypeName>uima.tcas.Annotation</supertypeName><features><featureDescription><name>language</name><description/><rangeTypeName>uima.cas.String</rangeTypeName></featureDescription></features></typeDescription>')
         self.assertEqual(castypesytem.getElementTag(castypesytem.getTypeDescElements()[0]),
                          '{http://uima.apache.org/resourceSpecifier}typeDescription')
@@ -47,41 +51,45 @@ class TestTypeSystemParserMethods(unittest.TestCase):
                          'http://uima.apache.org/resourceSpecifier')
         self.assertEqual(castypesytem.getElementLocalname(castypesytem.getTypeDescElements()[0]),'typeDescription')
         self.assertEqual(castypesytem.getElementText(castypesytem.getTypeDescElements()[0]), '\n            ')
-    def test_getTypeDescriptionName(self):        
+
+    def test_getTypeDescriptionName(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')                        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         "print(castypesytem.getTypeDescriptionName(castypesytem.getTypeDescElements()[0]))"
         self.assertEqual(castypesytem.getElementText(castypesytem.getTypeDescriptionName(castypesytem.getTypeDescElements()[0])),
               'uima.tcas.DocumentAnnotation')
 
-    def test_getTypeDescriptionDescription(self):        
+    def test_getTypeDescriptionDescription(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         "print(castypesytem.getTypeDescriptionDescription(castypesytem.getTypeDescElements()[0]))"
+
     def test_getTypeDescriptionSuperType(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')                        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         self.assertEqual(castypesytem.getElementTag(castypesytem.getTypeDescriptionSuperType(castypesytem.getTypeDescElements()[0])),
                          '{http://uima.apache.org/resourceSpecifier}supertypeName')
         self.assertEqual(castypesytem.getElementText(castypesytem.getTypeDescriptionSuperType(castypesytem.getTypeDescElements()[0])),
                          'uima.tcas.Annotation')
+
     def test_getTypeDescriptionFeatures(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')                        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         "print(castypesytem.getTypeDescriptionFeatures(castypesytem.getTypeDescElements()[0]))"
         self.assertEqual(castypesytem.getElementTag(castypesytem.getTypeDescriptionFeatures(castypesytem.getTypeDescElements()[0])),
                          '{http://uima.apache.org/resourceSpecifier}features')
         """how to iterate type description elements
-        for child in castypesytem.getTypeDescElements(): 
+        for child in castypesytem.getTypeDescElements():
             print(castypesytem.getElementText(castypesytem.getTypeDescriptionName(child)))
             print(castypesytem.getElementTag(castypesytem.getTypeDescriptionDescription(child)))
             print(castypesytem.getElementTag(castypesytem.getTypeDescriptionSuperType(child)))
             print(castypesytem.getTypeDescriptionFeatures(child))
             print(' ')
         """
+
     def test_getTypeDescriptionXXXAsString(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         typeDescElem = castypesytem.getTypeDescElements()[0]
         self.assertEqual(castypesytem.getTypeDescriptionNameAsString(typeDescElem),
               '<name xmlns="http://uima.apache.org/resourceSpecifier">uima.tcas.DocumentAnnotation</name>')
@@ -91,16 +99,17 @@ class TestTypeSystemParserMethods(unittest.TestCase):
               '<supertypeName xmlns="http://uima.apache.org/resourceSpecifier">uima.tcas.Annotation</supertypeName>')
 
         """how to iterate type description elements
-        for child in castypesytem.getTypeDescElements(): 
+        for child in castypesytem.getTypeDescElements():
             print(castypesytem.getTypeDescriptionNameAsString(child))
             print(castypesytem.getTypeDescriptionDescriptionAsString(child))
             print(castypesytem.getTypeDescriptionSuperTypeAsString(child))
             print(castypesytem.getTypeDescriptionFeaturesAsString(child))
             print(' ')
         """
+
     def test_getTypeDescriptionFeaturesAsString(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         "print(castypesytem.getTypeDescriptionFeaturesAsString(castypesytem.getTypeDescElements()[1]))"
         self.assertEqual(castypesytem.getElementTag(castypesytem.getTypeDescriptionFeatures(castypesytem.getTypeDescElements()[1])),
                          '{http://uima.apache.org/resourceSpecifier}features')
@@ -118,18 +127,19 @@ class TestTypeSystemParserMethods(unittest.TestCase):
         castypesytem2 = TypeSystemParser()
         """
         "print(castypesytem2.getTypeDescriptionFeaturesAsString(atypeDesc))"
-   
-    def test_findTypeDescElementByName(self):        
+
+    def test_findTypeDescElementByName(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')
-        docmetadata = castypesytem.findTypeDescElementByName('de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData')        
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
+        docmetadata = castypesytem.findTypeDescElementByName('de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData')
         self.assertEqual(castypesytem.getElementText(castypesytem.getTypeDescriptionName(docmetadata)),
               'de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData')
+
     def test_findTypeDescFeatureByName(self):
         castypesytem = TypeSystemParser()
-        castypesytem.setTypeAsFile('typesystem.xml')
+        castypesytem.setTypeAsFile('tests/testing_data/typesystem.xml')
         fd = castypesytem.findTypeDescFeatureByName('language','de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData')
-        
+
         self.assertEqual(castypesytem.getElementText(castypesytem.getFeatureDescName(fd)),'language')
         with self.assertRaises(AttributeError):
             castypesytem.getElementText(None)
